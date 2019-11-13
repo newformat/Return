f2fb0c8101a5ad9df81f32519353640ad8045cf5
sncode <snypcode@gmail.com>
2019-11-14 01:30:40 +0300

    update code

--- start.py
+++ start.py
@@ -30,14 +30,9 @@ bot_2 = {
     'hit': 0,
     'specs':0
 }
-
+"""
 # мишень / 0 - мимо
 target = (0,1,2,3,4,5)
-
-# общая таблица рейтинга | имя | промахи | попадания | очков
-table_result = []
-
-
 
 
 # ПРОЦЕСС ИГРЫ
@@ -50,6 +45,8 @@ while (True):
     tm.sleep(1)
 
     # проверяем, куда попал Вася
+    print("Бот " + bot['name'] + " стрельнул...  ( 1 ( 2 ( 3 ) 4 ) 5 )")
+    tm.sleep(1)
     if bot_shot != 0:
         bot['hit'] = bot['hit'] + 1
         bot['specs'] = bot['specs'] + bot_shot
@@ -58,10 +55,12 @@ while (True):
         bot['miss'] = bot['miss'] + 1  # запись промахов в ОБЩИЙ результат за игры.
         print("Бот " + bot['name'] + " промазал...")
         tm.sleep(1)
-
+    bot['number of rounds'] -= 1
     print()
 
     # проверяем, куда попал Петя
+    print("Бот " + bot_2['name'] + " стрельнул... ( 1 ( 2 ( 3 ) 4 ) 5 )")
+    tm.sleep(1)
     if bot_2_shot != 0:
         bot_2['hit'] = bot_2['hit'] + 1
         bot_2['specs'] = bot_2['specs'] + bot_2_shot
@@ -70,16 +69,30 @@ while (True):
         bot_2['miss'] = bot_2['miss'] + 1  # запись промахов в ОБЩИЙ результат за игры.
         print("Бот " + bot_2['name'] + " промазал...")
         tm.sleep(1)
-
+    bot_2['number of rounds'] -= 1
 
     # Кол-во патронов у ботов равно нулю / всего их 10
     if bot['number of rounds'] == 0 & bot_2['number of rounds'] == 0:
-        print('у ботов закончились патроны, игра окончена...')
+        print('\nу ботов закончились патроны, игра окончена...\n')
         break
-
+    else:
+        print("\nБот " + bot['name'] + "; кол-во патронов - " + str(bot['number of rounds']))
+        print("Бот " + bot_2['name'] + "; кол-во патронов - " + str(bot_2['number of rounds']) + '\n')
+"""
 
 # результаты
 tm.sleep(1)
 print('Результат - ')
-print('имя' + "\t" + "промахи"  + "\t" + "попадения"  + "\t" + "очки" )
-#print(result[0] + "\t\t" + str(result[1])  + "\t\t" + str(result[2])  + "\t\t" + str(result[3]) )
+print("имя\t\tпромахи\tпопадения\tочки" )
+tm.sleep(1)
+print(bot['name'] + "\t\t" + str(bot['miss']) + "\t\t" + str(bot['hit'])  + "\t\t" + str(bot['specs']))
+print(bot_2['name'] + "\t\t" + str(bot_2['miss']) + "\t\t" + str(bot_2['hit'])  + "\t\t" + str(bot_2['specs']))
+tm.sleep(1)
+
+# кто выйиграл?
+if bot['specs'] > bot_2['specs']:
+    print("Бот " + bot['name'] + " выиграл!")
+elif bot['specs'] < bot_2['specs']:
+    print("Бот " + bot_2['name'] + " выиграл!")
+else:
+    print('ничья...')
